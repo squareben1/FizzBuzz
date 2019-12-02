bad11226153116a1133ef92b28695280a5b7ebd2
Squareben1 <bengittins@live.com>
2019-12-02 17:01:37 +0000

    FizzBuzz and Test Complete

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,3 +1,10 @@
 def fizzbuzz(n)
-  'fizz'
+  if n % 3 == 0 && n % 5 == 0 
+    'FizzBuzz'
+  elsif n % 3 == 0 
+    'Fizz'
+  elsif n % 5 == 0 
+    'Buzz'
+  else return n
+  end 
 end 
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,7 +1,16 @@
 require 'fizzbuzz'
 
 describe 'fizzbuzz' do 
-  it 'returns "fizz" when passed 3' do
-    expect(fizzbuzz(3)).to eq 'fizz'
+  it 'returns "FizzBuzz" when passed 15' do
+    expect(fizzbuzz(15)).to eq 'FizzBuzz'
+  end 
+  it 'returns "Fizz" when passed 3' do
+    expect(fizzbuzz(3)).to eq 'Fizz'
+  end 
+  it 'returns "Buzz" when passed 5' do
+    expect(fizzbuzz(5)).to eq 'Buzz'
   end 
 end 
+
+# [1..100].each do | i |
+#   if i % 3 == 0 puts fizz - if 5 == 0 - 3 && 5 == 0  puts fizzbuzz }
